9c42bdbaf11d3cea9eb16e38cca0c85550e7781b
Pawelok35 <131856632+Pawelok35@users.noreply.github.com>
2024-08-19 22:52:37 +0200

    remove comments

--- js/script.js
+++ js/script.js
@@ -9,37 +9,14 @@ function getMoveName(argMoveId){
     printMessage('Nie znam ruchu o id ' + argMoveId + '.');
     return 'nieznany ruch'
 }
-
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-
 console.log('Wylosowana liczba to: ' + randomNumber);
-
 let computerMove = getMoveName (randomNumber);
-/*if(randomNumber == 1){
-    computerMove = 'kamień';
-} else if (randomNumber == 2){
-    computerMove = 'papier';
-} else if (randomNumber == 3){
-    computerMove = 'nożyce'
-}
-printMessage('Mój ruch to: ' + computerMove); */
-
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3:nożyce')
 
 console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = getMoveName (playerInput);
-
-/*if(playerInput == 1){
-    playerMove = 'kamień';
-} else if (playerInput == 2){
-    playerMove = 'papier';
-} else if (playerInput == 3){
-    playerMove = 'nożyce'
-} 
-printMessage('Twój ruch to: ' + playerMove);*/
-
-
 function displayResult(argComputerMove, argPlayerMove){
     console.log('Moves:', argComputerMove,' - ', argPlayerMove);
     printMessage('Mój ruch to ' + argComputerMove + ', Twój ruch to ' + argPlayerMove);
@@ -59,3 +36,4 @@ if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
   }
 }
 displayResult(computerMove, playerMove)
+
