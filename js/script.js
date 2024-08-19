e0a1943efa36814b984a1d5c9429f42db3389716
Pawelok35 <131856632+Pawelok35@users.noreply.github.com>
2024-08-19 19:32:55 +0200

    fixed playerInput

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
-
 if(randomNumber == 1){
     computerMove = 'kamień';
 } else if (randomNumber == 2){
@@ -17,17 +16,14 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3:nożyce')
 
 console.log('Gracz wpisał: ' + playerInput);
 
-
-
 let playerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if(playerInput == 1){
     playerMove = 'kamień';
-} else if (randomNumber == 2){
+} else if (playerInput == 2){
     playerMove = 'papier';
-} else {
+} else if (playerInput == 3){
     playerMove = 'nożyce'
 }
-printMessage('Mój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove);
 
-i
