a6ec1933772d58b8b037d56522e3bcbb62da376c
Pawelok35 <131856632+Pawelok35@users.noreply.github.com>
2024-08-19 18:52:35 +0200

    remove second printmessage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 let computerMove = 'kamień';
 let playerMove = 'papier';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to ' + computerMove + ' to przegrywasz!');
+
